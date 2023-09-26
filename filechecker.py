7410f188ecd6bfc4a96f178f3908c2ddfeda45d0
TomB <43079538+Tomaszbrauntsch@users.noreply.github.com>
2023-09-26 03:46:02 -0400

    updated-name-and-added-better-visuals

--- filechecker.py
+++ filechecker.py
@@ -5,6 +5,7 @@ import io # allows for text manipulation of bytes
 import json # used to look at the magicnumbers
 import re # used for comparsion of file headers in json file
 import sys # used to read arguments
+import pyfiglet
 def byteExtraction(fileName):
     photoBytes = [] #used later for extracting bytes from data
     bytesString = ""
@@ -44,22 +45,35 @@ def byteChecker(fileName):
         if(re.search(fileHeadersReference, extractedBytes)):
             found = 1
     else:
+        print("-"*68)
         print("This file extension is either not in our database or doesn't exist!")
+        print("-"*68)
     
     if (found == 1):
-        print("This file is correct :: " + str(fileExt))
+        print("-"*43)
+        print("✓\t" + str(fileExt) + " is the correct file type")
+        print("-"*43)
     elif(found == 0):
-        print("The file is either corrupted or the wrong file extension")
+        print("-"*63)
+        print("X\tThe file is either corrupted or the wrong file extension")
+        print("-"*63)
 
 def main():
+    print()
+    ascii_banner = pyfiglet.figlet_format("FILE CHECKER")
+    print(ascii_banner)
     try:
         fileName = sys.argv[1]
         byteChecker(fileName)
     except IndexError:
+        print("-"*57)
         print("MISSING ARG ERROR: Please include a file as an argument!")
+        print("-"*57)
         exit(2) #missing arg error
     except FileNotFoundError:
+        print("-"*50)
         print("INVALID FILE ERROR: Please include a valid file!")
+        print("-"*50)
         exit(3) #invalid file error
     
 
